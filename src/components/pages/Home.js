bae7fe6f253363bd59cc14d2645a694914d27907
michaeljoy255 <kodiakchaos@gmail.com>
2020-02-13 16:35:04 -0500

    added button to clear local storage

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,11 +5,20 @@ import Routines from "../home/Routines";
 const Home = () => {
   const appTitle = "Fitness Progression";
 
+  const clearLocalStorage = function() {
+    if (window.confirm("Clear routine records from Local Storage?")) {
+      localStorage.clear();
+    }
+  };
+
   return (
     <Fragment>
       {/* <Navbar /> */}
       <h1 className='title'>{appTitle}</h1>
       <Routines />
+      <button className='btn clear' onClick={clearLocalStorage}>
+        Clear Local Storage
+      </button>
     </Fragment>
   );
 };
